4b1b5c6fd8df99d64b8607f7832a1e0ed43c7003
NikolaykoSergey <martiksscat@gmail.com>
2025-10-12 18:08:50 +0300

    Update task11.py

--- task11.py
+++ task11.py
@@ -1,12 +1,19 @@
+#  todo: Дан номер месяца (1 — январь, 2 — февраль, ...). Вывести название соответствующего
+#  времени года ("зима", "весна" и т.д.).
+
+
 x :int = int(input('Введите число месяца: '))
 if x == 0 or x > 12:
     print('Error')
     exit(100)
-if 1 <= x <= 2 or x == 12:
-    print('Зима')
-elif 2 < x < 6:
-    print('Весна')
-elif 5 < x < 9:
-    print('Лето')
-elif x > 8 or x < 12:
-    print('Осень')
+def times():
+    if 1 <= x <= 2 or x == 12:
+        return ('Зима')
+    elif 2 < x < 6:
+        return ('Весна')
+    elif 5 < x < 9:
+        return ('Лето')
+    else:
+        return ('Осень')
+rez = times()
+print(f"время года: {rez}")
